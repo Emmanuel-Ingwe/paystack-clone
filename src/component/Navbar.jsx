0afdc73abf08425c1bc0cdcfe397946faf67002c
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-18 23:58:58 +0100

    nav

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = (props) => {
   return (
-    <div className="m-20">
+    <nav className="m-20">
         <h1>Paystack</h1>
-    </div>
+    </nav>
 
   )
 }
