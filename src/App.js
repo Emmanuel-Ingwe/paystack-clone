3f23c9e8402050295f76d33505d734b7b7a4dfa0
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-21 06:47:30 +0100

    hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 const App = () => {
   return (
-    <div className=" w-full h-full overflow-hidden">
+    <div className=" w-full h-full overflow-hidden bg-green-200">
       <Navbar />
+      <Hero />
 
       {/* <BrowserRouter>
         <Routes>
