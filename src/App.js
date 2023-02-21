6492b86b74435098bb9ae574f462e497094bf8f6
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-21 07:12:31 +0100

    ++

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
+
+import './App.css';
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 const App = () => {
   return (
-    <div className=" w-full h-full overflow-hidden bg-green-200">
+    <div className="App w-full h-full overflow-hidden bg-green-200">
       <Navbar />
       <Hero />
 
