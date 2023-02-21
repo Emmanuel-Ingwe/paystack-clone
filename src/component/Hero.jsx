2efaccc461c20894d62ad99c79065aefb3917943
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-21 09:02:55 +0100

    __

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -3,13 +3,20 @@ import React from "react";
 const Hero = () => {
 	return (
 		<div className='m-40 mt-40'>
-			<h1 className='color text-5xl font-bold leading-snug'>
+			<h1 className='color text-5xl font-bold leading-tight'>
 				Modern online and offline <br /> payments for Africa
 			</h1>
-			<p>
-				Paystack helps businesses in Africa get paid by anyone, anywhere in the
-				world
+			<p className='text-xl mt-5'>
+				Paystack helps businesses in Africa get paid by anyone, <br /> anywhere
+				in the world
 			</p>
+
+			<div className='mt-8'>
+				<button className='px-9 p-1.5 bg-green-500 text-white rounded-lg leading-10 font-semibold'>
+					Create a free account
+				</button>
+				<button>or Contact Sales</button>
+			</div>
 		</div>
 	);
 };
